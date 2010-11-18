f8a57aaa3a5b93ebbc425525fa4c628b0b399a36
Ahmed Mahmoud <amaiko@Amaiko-PC.(none)>
2010-11-18 15:24:21 +0200

    1.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+?>
+
+<a href='add.php'>ADD</a><br />
+<a href='admin.php'>Admin Area</a><br />
+<a href='user.php'>My Account</a><br />
+<form action='result.php' method='POST'>
+<input type='text' name='uni_num' />
+<input type='submit' value='GET' />
+</form>
+</body>
+</html>
+
+
